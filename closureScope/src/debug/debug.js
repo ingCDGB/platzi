17c286e0d11562d9fde5b5e39294aedf15544a8c
Carlos <ing.carlosgarciabendahan@gmail.com>
2020-08-10 11:27:18 -0400

    DEBUGGER

---
+++ closureScope/src/debug/debug.js
@@ -0,0 +1,14 @@
+var a = 'hello'
+
+function hello () {
+    let b = 'hello world'
+
+    const c = 'hello wordl!!!'
+
+    if(true){
+        let d = 'HELLO WORLD'
+        debugger
+    }
+}
+
+hello()
