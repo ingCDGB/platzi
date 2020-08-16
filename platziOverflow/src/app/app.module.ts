3604ab56b6315ce9b449be0c37f8bc27b3d10f15
Carlos <ing.carlosgarciabendahan@gmail.com>
2020-08-16 14:37:52 -0400

    CONECTANDO NUESRAS PANTALLAS CON EL ROUTER DE ANGULAR

--- platziOverflow/src/app/app.module.ts
+++ platziOverflow/src/app/app.module.ts
@@ -22,6 +22,8 @@ import { QuestionFormComponent } from './question/question-form.component';
 
 import { MomentModule } from 'angular2-moment';
 
+import { Routing } from './app.routing';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -38,7 +40,8 @@ import { MomentModule } from 'angular2-moment';
     MaterialModule,
     MomentModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    Routing
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ platziOverflow/src/app/app.routing.ts
@@ -0,0 +1,23 @@
+import { Routes, RouterModule } from '@angular/router';
+import { QuestionListComponent } from './question/question-list.component';
+import { SigninScreenComponent } from './auth/signin-screen.component';
+import { RegistryScreenComponent } from './registry/registry-screen.component';
+/**
+ * Agregamos nuestras rutas
+ * Cada una de ellas será un objeto JavaScript
+ */
+const APP_ROUTES: Routes = [
+    /**
+     * si no colocamos pathMatch full al ser un string vacio la primera ruta
+     * va a seguir validando con las otras rutas y al hacer match con la 1ra 
+     * pero las demás rutas también comienzan entonces seguirá validando
+     * con esto valida que luego del / hay string vacio
+     */
+    { path: '', component: QuestionListComponent, pathMatch: 'full' }, 
+    { path: 'signin', component: SigninScreenComponent },
+    { path: 'signup', component: RegistryScreenComponent }
+];
+
+//Nuestro sistema de ruta para nuestra aplicación
+export const Routing = RouterModule.forRoot(APP_ROUTES);
+
